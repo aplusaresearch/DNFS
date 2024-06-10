732b8cd5364bda3565f6541ed626032105fddffa
Simon JARICOT <s.jaricot@aplusaresearch.com>
2024-06-10 14:45:05 +0200

    Update

--- flask/server.py
+++ flask/server.py
@@ -10,5 +10,10 @@ def hello():
     return "Hello!\n"
 
 
+@app.route('/world')
+def hello_w():
+    return "Hello world!\n"
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=os.environ.get("FLASK_SERVER_PORT", 9090), debug=True)
